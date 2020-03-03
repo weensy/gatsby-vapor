09cf0363382ae832d2b93632620a8055464f3f21
vaporwavy <i@ttv8.me>
2020-03-03 10:32:26 +0900

    Fix logo css

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,98 +7,52 @@ import Search from "./search"
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        // <h1
-        //   style={{
-        //     // ...scale(1.5),
-        //     // marginBottom: rhythm(1.5),
-        //     // marginTop: 0,
-        //   }}
-        // >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        // </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            // fontFamily: `Montserrat, sans-serif`,
-            // marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+    
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <ul className="test">
-          <li>
-            <header>{header}</header>
-          </li>
-          <li>
-            Tags
-          </li>
-          <li>
-            Search
-          </li>
-          <li>
-            <div className="toggler">
-              <ThemeToggler>
-                {({ theme, toggleTheme }) => (
-                  <label className="tog">
-                    <input
-                      type="checkbox"
-                      onChange={e =>
-                        toggleTheme(e.target.checked ? "dark" : "light")
-                      }
-                      checked={theme === "dark"}
-                      className="tttt"
-                    />
-                    {theme === "dark" ? (
-                      <div className="abc">
-                        Light
-                      </div>
-                    ) : (
-                      <div className="abc">
-                        Dark
-                      </div>
-                    )}
-                  </label>
-                )}
-              </ThemeToggler>
-            </div>
-          </li>
-        </ul>
+      <div className="page">
+        <div className="header-container">
+          <Link
+            className="title"
+            to={`/`}
+          >
+            {title}
+          </Link>
+          <ul className="test">
+            <li>
+              Tags
+            </li>
+            <li>
+              Search
+            </li>
+            <li>
+              <div className="toggler">
+                <ThemeToggler>
+                  {({ theme, toggleTheme }) => (
+                    <label className="tog">
+                      <input
+                        type="checkbox"
+                        onChange={e =>
+                          toggleTheme(e.target.checked ? "dark" : "light")
+                        }
+                        checked={theme === "dark"}
+                        className="tttt"
+                      />
+                      {theme === "dark" ? (
+                        <div className="abc">
+                          Light
+                        </div>
+                      ) : (
+                        <div className="abc">
+                          Dark
+                        </div>
+                      )}
+                    </label>
+                  )}
+                </ThemeToggler>
+              </div>
+            </li>
+          </ul>
+        </div>
         <StaticQuery
               query={graphql`
                 query SearchIndexQuery {
