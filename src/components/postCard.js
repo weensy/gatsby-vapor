0f651dba58759ccf1f6b17c520bbaabefadc4464
vaporwavy <i@ttv8.me>
2020-04-14 15:37:29 +0900

    Fix css for pagenator

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -8,30 +8,36 @@ class PostCard extends Component {
     return (
       <article key={props.node.fields.slug}>
         <div className="post-card">
-        <Tags tags={props.node.frontmatter.tags}/>
-        <header>
-          <h1>
-            <Link to={props.node.fields.slug}>
-            {props.node.frontmatter.title || props.node.fields.slug}
-            </Link>
-          </h1>
-          <small>{props.node.frontmatter.date}</small>
-        </header>
-        {props.node.frontmatter.thumbnail &&
-          <img
-            alt={props.node.frontmatter.thumbnail.childImageSharp.fluid.originalName}
-            src={props.node.frontmatter.thumbnail.childImageSharp.fluid.src}
-          />
-        }
-        <section>
-          <p
-            dangerouslySetInnerHTML={{
-            __html: props.node.frontmatter.description || props.node.excerpt,
-            }}
-          />
-        </section>
-      </div>
-    </article>
+          <Tags tags={props.node.frontmatter.tags}/>
+          <header>
+            <h1>
+              <Link to={props.node.fields.slug}>
+              {props.node.frontmatter.title || props.node.fields.slug}
+              </Link>
+            </h1>
+            <small>{props.node.frontmatter.date}</small>
+          </header>
+          {props.node.frontmatter.thumbnail &&
+            <img
+              alt={props.node.frontmatter.thumbnail.childImageSharp.fluid.originalName}
+              src={props.node.frontmatter.thumbnail.childImageSharp.fluid.src}
+            />
+          }
+          <section>
+            <p
+              dangerouslySetInnerHTML={{
+              __html: props.node.frontmatter.description || props.node.excerpt,
+              }}
+            />
+          </section>
+          <div className="post-card-readmore">
+            <div>Read more</div>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xmlSpace="preserve">
+              <g><path d="M767.9,499.9L291.6,10l-59.4,61.3l416.6,428.7L232.1,928.7l59.5,61.3L767.9,499.9z"/></g>
+            </svg>
+          </div>
+        </div>
+      </article>
     );
   }
 }
--- src/templates/blog-index.js
+++ src/templates/blog-index.js
@@ -33,12 +33,28 @@ class BlogIndexTemplate extends React.Component {
         {lastPage === 1 || 
         <div className="pagenator">
           {currentPage === 1
-            ?<div className="pagenator-unlinked">{`<`}</div>
-            :<Link className="pagenator-link" to={prevPage === 1 ?`/` :`/` + prevPage}>{`<`}</Link>}
+            ? <div className="pagenator-unlinked">
+                <svg transform="scale(-1, 1)" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xmlSpace="preserve">
+                  <g><path d="M767.9,499.9L291.6,10l-59.4,61.3l416.6,428.7L232.1,928.7l59.5,61.3L767.9,499.9z"/></g>
+                </svg> 
+              </div>
+            : <Link className="pagenator-link" to={prevPage === 1 ?`/` :`/` + prevPage}>
+                <svg transform="scale(-1, 1)" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xmlSpace="preserve">
+                  <g><path d="M767.9,499.9L291.6,10l-59.4,61.3l416.6,428.7L232.1,928.7l59.5,61.3L767.9,499.9z"/></g>
+                </svg> 
+              </Link>}
           <div>Page {currentPage}</div>
           {currentPage === lastPage
-            ?<div className="pagenator-unlinked">{`>`}</div>
-            :<Link className="pagenator-link" to={`/` + nextPage}>{`>`}</Link>}
+            ? <div className="pagenator-unlinked">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xmlSpace="preserve">
+                  <g><path d="M767.9,499.9L291.6,10l-59.4,61.3l416.6,428.7L232.1,928.7l59.5,61.3L767.9,499.9z"/></g>
+                </svg>
+              </div>
+            : <Link className="pagenator-link" to={`/` + nextPage}>
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xmlSpace="preserve">
+                  <g><path d="M767.9,499.9L291.6,10l-59.4,61.3l416.6,428.7L232.1,928.7l59.5,61.3L767.9,499.9z"/></g>
+                </svg>
+              </Link>}
         </div>}
       </Layout>
     )
